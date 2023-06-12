0fb62a1670cca622aaa92cf09fe6220f65a959af
Anna <annklimova10@gmail.com>
2023-06-12 23:06:48 +0000

    Add admin views

--- fashion/admin.py
+++ fashion/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Article, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Article)
+class SiteAdmin(SummernoteModelAdmin):
+
+    ist_display = ('title', 'slug', 'status', 'created_at')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_at', 'approved')
+    list_filter = ('approved', 'created_at')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- fashionista/settings.py
+++ fashionista/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'fashion',
 ]
 

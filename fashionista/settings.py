05a9340f94d95e9d098905b53525572fb06d5baf
Anna <annklimova10@gmail.com>
2023-06-13 11:46:45 +0000

    Add envy.py

--- fashionista/settings.py
+++ fashionista/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-annausernam-fashionnews-xx4vemgsrc4.ws-eu99.gitpod.io', 'fashionista.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-annausernam-fashionnews-dtdx5l4f8yl.ws-eu99.gitpod.io', 'fashionista.herokuapp.com', 'localhost']
 
 
 # Application definition

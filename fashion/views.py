6464285c514a9c65effc322eb4cf02b0b277825b
Anna <annklimova10@gmail.com>
2023-06-13 14:06:18 +0000

    Add comments and likes views

--- fashion/views.py
+++ fashion/views.py
@@ -10,4 +10,22 @@ class ArticleList(generic.ListView):
     template_name = "index.html"
     paginate_by = 6
 
+class ArticleDetail(View):
+    
+    def get(self, request, slug, *args, **kwags):
+        queryset = Article. objects.filter(status=1)
+        article = get_object_or_404(queryset, slug=slug)
+        comments = arrticle.filter(approved=True).order_by('-created_at')
+        liked = False
+        if article.likes.filter(id=self.request.userr.id).exists():
+            liked = True
 
+        return render(
+            request,
+            "article_detail.html"
+            {
+                "article": article
+                "comments": comments
+                "liked": liked
+            },
+        )

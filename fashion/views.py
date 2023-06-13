9470b2dd24f2831054e2908a630c11358ca48e97
Anna <annklimova10@gmail.com>
2023-06-13 13:18:08 +0000

    Add a basic structure to html files and create css and urls.py inside the app directory

--- fashion/views.py
+++ fashion/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Article
-from .forms import CommentForm
 
 
 class ArticleList(generic.ListView):
     model = Article
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    queryset = Article.objects.filter(status=1).order_by("-created_at")
     template_name = "index.html"
     paginate_by = 6
 
--- fashionista/urls.py
+++ fashionista/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("fashion.urls"), name="fashion-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
